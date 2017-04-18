4f60c89b0fec9bd2058c4a53c728d6cb5119a5c5
Aaron Kucyi <aaron.kucyi@gmail.com>
2017-04-18 13:58:30 -0700

    plotting rs-FC

--- view_BOLD_sbca_elecs.m
+++ view_BOLD_sbca_elecs.m
@@ -5,12 +5,37 @@ Runs=cellstr(runs);
 
 globalECoGDir=getECoGSubDir;
 fsDir=getFsurfSubDir();
+cd([fsDir '/' Patient '/elec_recon']);
+coords=dlmread([Patient '.PIALVOX'],' ',2,0);
+cd electrode_spheres;
+mkdir('SBCA/figs');
 
-%cd([fsDir '/' Patient '/elec_recon']);
+parcOut=elec2Parc([Patient]);
+elecNames = cell(length(parcOut),1);
 
+for run=1:total_runs;
+    run_num=runs;
+for elec=1:length(coords);
+elec_num=num2str(elec);
+elec_name=char(parcOut(elec,1));
+    elec_ts=load(['elec' elec_num run_num '_ts_PIALVOX.txt']);    
+    if elec_ts(1)~=0 % ignore WM electrodes
+        
+% Make color matrix
+color_matrix = ones(length(parcOut),3)*0.5; % make all electrodes gray
+color_matrix(elec,:)=0;
 
 cfg=[];
 cfg.view='lomni';
 cfg.olayUnits='z';
-cfg.pialOverlay=[fsDir '/' Patient '/elec_recon/electrode_spheres/SBCA/elec1run1_LH.mgh']
+cfg.pullOut=3;
+%cfg.elecColorScale =[0 1];
+cfg.onlyShow={elec_name};
+%cfg.elecColors= color_matrix(:,1:3);   
+cfg.pialOverlay=[fsDir '/' Patient '/elec_recon/electrode_spheres/SBCA/elec' elec_num run_num '_LH.mgh']
 cfgOut=plotPialSurf(Patient,cfg);
+print('-opengl','-r300','-dpng',strcat([pwd,filesep,'SBCA',filesep,'figs',filesep,'BOLD_FC_',elec_name]));
+close;
+    end
+end
+end
