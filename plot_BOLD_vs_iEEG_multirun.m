0ea046f0e4c6d5cb9f45c1dc26b84c799acbd80c
Aaron Kucyi <aaron.kucyi@gmail.com>
2017-10-19 15:02:46 -0700

    group plot partial corr vs BOLD

--- plot_BOLD_vs_iEEG_multirun.m
+++ plot_BOLD_vs_iEEG_multirun.m
@@ -1,5 +1,5 @@
 % must first run BOLD_vs_ECoG_FC_corr_iElvis.m for each run used
-% dFC_preproc_list.txt file should contain subject name (column 1), sub number (column 2),
+% dFC_replicate.txt file should contain subject name (column 1), sub number (column 2),
 % electrode number (column 3), and network identity (column 4), run numbers (columns 5+6) 
 % network identity: 1=DMN, 2=DAN, 3=FPCN
 
@@ -37,11 +37,13 @@ for sub=1:length(subjects)
     ecog_run2name=num2str(ecog_run2(sub));
     elec=elecs(sub)
     network=networks(sub);
-%% Load corr values for each run
+%% Load corr and partial corr values for each run
 cd([globalECoGDir '/Rest/' Patient '/Run' ecog_run1name '/BOLD_ECoG_figs/GSR']);
 run1_HFB_medium=load('corr_BOLD_HFB_medium_allelecs.mat');
+run1_HFB_medium_partial=load('partialcorr_BOLD_HFB_medium_allelecs.mat');
 cd([globalECoGDir '/Rest/' Patient '/Run' ecog_run2name '/BOLD_ECoG_figs/GSR']);
 run2_HFB_medium=load('corr_BOLD_HFB_medium_allelecs.mat');
+run2_HFB_medium_partial=load('partialcorr_BOLD_HFB_medium_allelecs.mat');
 
 %% get elec names
 fsDir=getFsurfSubDir();
@@ -52,8 +54,12 @@ elecNames = parcOut(:,1);
 seed_HFB_medium_run1=run1_HFB_medium.corr_BOLD_HFB_medium_allelecs(elec);
 seed_HFB_medium_run2=run2_HFB_medium.corr_BOLD_HFB_medium_allelecs(elec);
 
+seed_HFB_medium_run1_partial=run1_HFB_medium_partial.partialcorr_BOLD_HFB_medium_allelecs(elec);
+seed_HFB_medium_run2_partial=run2_HFB_medium_partial.partialcorr_BOLD_HFB_medium_allelecs(elec);
+
 %% concatenate across subjects
 allsubs_seed_HFB_medium_allruns(:,sub)=[seed_HFB_medium_run1 seed_HFB_medium_run2];
+allsubs_seed_HFB_medium_allruns_partial(:,sub)=[seed_HFB_medium_run1_partial seed_HFB_medium_run2_partial];
 end
 
 %% Make plots
@@ -84,7 +90,7 @@ for i=1:length(subject_nums)
     end
 end
 
-% plot
+% plot corr
 FigHandle = figure('Position', [400, 600, 400, 700]);
 figure(1)
 for i=1:length(allsubs_seed_HFB_medium_allruns)
@@ -113,4 +119,32 @@ print('-opengl','-r300','-dpng',strcat([pwd,filesep,'BOLD_vs_ECoG_multirun']));
 end
 pause; close;
 
+%plot partial corr (distance-corrected)
+FigHandle = figure('Position', [400, 600, 400, 700]);
+figure(1)
+for i=1:length(allsubs_seed_HFB_medium_allruns)
+    plot(1:size(allsubs_seed_HFB_medium_allruns_partial,1),allsubs_seed_HFB_medium_allruns_partial(:,i),[subjectmarker{i,:} '-'], ...
+        'LineWidth',1,'Color',network_color(i,:),'MarkerFaceColor',network_color(i,:), ...
+        'MarkerSize',8,'MarkerEdgeColor',network_color(i,:));      
+    ylim([0 0.8]);
+    xlim([0.5 2.5]);
+       set(gca,'Xtick',1:1:3)
+ set(gca,'XTickLabel',{'','1', '2'})
+ set(gca,'Fontsize',18,'FontWeight','bold','LineWidth',2,'TickDir','out');
+  %ylabel('BOLD-ECoG FC partial correlation (r)'); 
+  
+    hold on
+   set(gca,'box','off'); 
+set(gca,'Fontsize',18,'FontWeight','bold','LineWidth',2,'TickDir','out');
+set(gcf,'color','w');
+%title({[elec_name ': BOLD FC vs iEEG FC']},'Fontsize',12);
+  ylim([0 0.8]);
+  xlim([0.5 2.5]);
+   set(gca,'Xtick',0:1:8)
+ set(gca,'XTickLabel',{'', '1','2'})
+ylabel('Partial Correlation with BOLD FC (r)');
+xlabel('ECoG Run');
+print('-opengl','-r300','-dpng',strcat([pwd,filesep,'BOLD_vs_ECoG_multirun_partialcorr']));
+end
+pause; close;
 
